6c31a27d9f0300723bf98d3f7792596d903e8d1e
Simon Batty <simon@psymon.co.uk>
2017-10-16 11:10:22 +0100

    More header-side-nav development.

--- src/_modules/textinput/textinput.js
+++ src/_modules/textinput/textinput.js
@@ -1,21 +1,23 @@
 export default class TextInput {
   constructor() {
-  	this.emailInput = document.querySelector('input[type="email"]')
-  	this.emailInput.addEventListener('focusout', () => {
-  		if(!this.validateEmail(this.emailInput.value)) {
-  			this.emailInput.classList.add('error')
-  		} else {
-  			this.emailInput.classList.remove('error')
-  		}
-  	})
+    this.emailInput = document.querySelector('input[type=“email”]')
+    if (this.emailInput) {
+      this.emailInput.addEventListener('focusout', () => {
+        if (!this.validateEmail(this.emailInput.value)) {
+          this.emailInput.classList.add('error')
+        } else {
+          this.emailInput.classList.remove('error')
+        }
+      })
+    }
   }
 
   validateEmail(email) {
-  	if(email.length < 1) {
-	    return true
-	  } else {
-	  	const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-	    return re.test(email);
-	  }
-	}
+    if (email.length < 1) {
+      return true
+    } else {
+      const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      return re.test(email);
+    }
+  }
 }
