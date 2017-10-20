619b59c86651178c4b1a26afe0d8229a4e59606c
Simon Batty <simon@psymon.co.uk>
2017-10-20 17:31:31 +0100

    Global variable changes.

--- src/_scripts/main.js
+++ src/_scripts/main.js
@@ -1,54 +1,15 @@
 // Main javascript entry point
 // Should handle bootstrapping/starting application
-
-// import { TweenLite } from 'gsap'
-// import ScrollToPlugin from 'gsap/ScrollToPlugin'
-// import Team from '../_modules/team/team'
 import Menu from '../_modules/menu/menu'
 import Nav from '../_modules/nav/nav'
-
-// import Countdown from '../_modules/countdown/countdown'
-// import SideNav from '../_modules/side-nav/side-nav'
-// import PageControls from '../_modules/page-controls/page-controls'
 import TextInput from '../_modules/textinput/textinput'
 import Slider from '../_modules/slider/slider'
 
-// function protoMessage() {
-//   document.querySelector('.prototype-message').addEventListener('click', (e) => {
-//     e.currentTarget.style.display = 'none'
-//   })
-// }
 
 class Main {
   constructor() {
     // super()
-    // this.body = document.getElementsByTagName('body')[0]
-    // this.mainHeader = document.getElementsByClassName('main-header')[0]
-    // this.mainLogo = document.getElementsByClassName('main-header__logo')[0]
-    // this.burger = document.getElementById('main-burger')
-    // this.subNav = document.getElementsByClassName('sub-nav')[0]
-    // this.subNavLinks = document.querySelectorAll('[data-page-link]')
-    // this.subNavMenuLinks = document.querySelectorAll('.sub-nav__item [data-page-link]')
-    // this.fullSections = document.querySelectorAll('.site-contents__full:not(.site-contents__full--between)')
-    // this.sideNavs = document.getElementsByClassName('side-nav')
-    // this.carousels = document.getElementsByClassName('carousel')
-
-    // this.mobileWidth = 960
-    // this.supportOffset = window.pageYOffset !== undefined
-    // this.lastKnownPos = 0
-    // this.ticking = false
-    // this.wheelTicking = false
-    // this.scrollDir
-    // this.pageBumpTimeOut = 4500
-
-
-    // this.resizeTimer
-    // this.pageBumpTimer
-    // this.setDimensions()
-    // this.resizeEvent()
-    // this.scrollEvent()
-    // this.navChangeOver(document.body.scrollTop)
-    // this.subMenuScrollTo()
+    this.navs = document.getElementsByClassName('toggle-nav')
 
     // protoMessage()
     this.textInput = new TextInput()
@@ -59,7 +20,15 @@ class Main {
   initSections() {
     // TODO: A better way of initating these modules.
     new Menu()
-    new Nav()
+
+    const toggleNavs = []
+    Array.from(this.navs).forEach((foundNav) => {
+      if (foundNav.classList.contains('toggle-nav--animated-height')) {
+        toggleNavs.push(new Nav({ animatedHeight: true, parent: foundNav }))
+      } else {
+        toggleNavs.push(new Nav({ parent: foundNav }))
+      }
+    })
   }
 }
 
