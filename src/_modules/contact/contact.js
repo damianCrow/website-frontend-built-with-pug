a933d348c9ac811d9108064d0fa063adc52d0461
Simon Batty <simon@psymon.co.uk>
2017-10-24 18:19:53 +0100

    Lintd contact.js, moved away from mapbox css CDN and imported directly from node_modules.

--- src/_modules/contact/contact.js
+++ src/_modules/contact/contact.js
@@ -2,19 +2,19 @@ import Mapbox from 'mapbox-gl'
 
 export default class Contact {
   constructor() {
-  	Mapbox.accessToken = 'pk.eyJ1IjoibXJhd2Vzb21lIiwiYSI6ImNqMzJzeDN4ZjAwMHAyd250YmRyZm04MDcifQ.YcsqDP3hqulR3f523AoB1g'
-		const map = new Mapbox.Map({
-			container: 'map',
-			center: [-0.10618954583290952, 51.51381053435017],
-			zoom: 16,
-			style: 'mapbox://styles/mapbox/dark-v9'
-		})
+    Mapbox.accessToken = 'pk.eyJ1IjoibXJhd2Vzb21lIiwiYSI6ImNqMzJzeDN4ZjAwMHAyd250YmRyZm04MDcifQ.YcsqDP3hqulR3f523AoB1g'
+    const map = new Mapbox.Map({
+      container: 'map',
+      center: [-0.10618954583290952, 51.51381053435017],
+      zoom: 16,
+      style: 'mapbox://styles/mapbox/dark-v9',
+    })
 
-		const marker = document.createElement('div')
-		marker.classList.add('marker')
+    const marker = document.createElement('div')
+    marker.classList.add('marker')
 
-		new Mapbox.Marker(marker)
+    new Mapbox.Marker(marker)
       .setLngLat([-0.10718954583290952, 51.51381053435017])
-      .addTo(map);
+      .addTo(map)
   }
 }
