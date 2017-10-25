4ca29beba790c1f2d80b0faf8bd57cef1a511ff8
Damian Whyte <damian.w@interstateteam.com>
2017-10-25 11:18:55 +0100

    slider changed to fade and links on contact page replaced with buttons

--- src/_modules/slider/slider.js
+++ src/_modules/slider/slider.js
@@ -4,13 +4,12 @@ import Slick from 'slick-carousel'
 export default class Slider {
   constructor() {
   	$('.slider-container').slick({
-		  slidesToShow: 1,
-		  slidesToScroll: 1,
 		  autoplay: true,
-		  autoplaySpeed: 10000,
+		  autoplaySpeed: 7000,
 		  arrows: false,
-		  speed: 1000,
+		  speed: 2000,
 		  dots: true,
+		  fade: true,
 		  customPaging: function(slick, index) {
         return `<div class="progress__wrap"><a class="progress__wrap__text">${(index + 1)}</a></div>`;
     	},
