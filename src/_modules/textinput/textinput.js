2501b702ac6e4780d7be8c8ec273567269ac187c
Damian Whyte <damian.w@interstateteam.com>
2017-10-26 12:43:41 +0100

    form validation added for all field types.

--- src/_modules/textinput/textinput.js
+++ src/_modules/textinput/textinput.js
@@ -1,6 +1,9 @@
+import $ from 'jquery'
+
 export default class TextInput {
   constructor() {
   	this.emailInput = document.querySelector('input[type="email"]')
+
     if(this.emailInput) {
     	this.emailInput.addEventListener('focusout', () => {
     		if(!this.validateEmail(this.emailInput.value)) {
@@ -12,6 +15,74 @@ export default class TextInput {
     		}
     	})
     }
+
+    $('.personal_form__form, .contact-form__form').submit((e) => {
+      e.preventDefault()
+
+      if($(e.target).find('.phone-validate')) {
+        let phoneNumber = ''
+
+        $('.phone-validate').find('input').each((idx, item) => {
+          phoneNumber += $(item).val().replace('+', '')
+        })
+        
+        if(!this.validatePhoneNumber(`+${phoneNumber}`)) {
+          $('.phone-validate').append('<b class="error-message">Invalid Phone Number!</b>');
+          $('.phone-validate').find('input').addClass('error').focus((ele) => {
+            $(ele.target).removeClass('error')
+            $('.phone-validate').find('.error-message').remove()
+          })
+        } else {
+          $('.phone-validate').find('input').removeClass('error')
+          $('.phone-validate').find('.error-message').remove()
+        } 
+      }
+
+      $(e.target).find('input:not(.error)').each((indx, field) => {
+        if($(field).val().length < 1) {
+          $(field).parent().append('<b class="error-message">Input Required!</b>');
+          $(field).addClass('error').focus((el) => {
+            $(el.target).removeClass('error')
+            $(el.target).parent().find('.error-message').remove()
+          })
+        } else {
+          $(field).removeClass('error')
+          $(field).parent().find('.error-message').remove()
+        }
+      })
+
+      if($(e.target).find('.dropdown')) {
+        $('.dropdown').each((indx, select) => {
+          if($(select).val() === null) {
+            $(select).addClass('error').change((el) => {
+              $(el.target).removeClass('error')
+            })
+          } else {
+            $(select).removeClass('error')
+          }
+        })
+      } 
+
+      if($(e.target).find('.radiowrap__radio')) {
+        let radioCategories = []
+
+        $('.radiowrap__radio').each((indx, radio) => {
+          radioCategories.push($(radio).attr('name'))
+        })
+
+        const radioArray = Array.from(new Set(radioCategories))
+
+        for(let i = 0; i < radioArray.length; i++) {
+          if($(`input[name=${radioArray[i]}]:checked`).length <= 0) {
+            $(`input[name=${radioArray[i]}]`).addClass('error').change((el) => {
+              $(el.target).removeClass('error')
+            })
+          } else {
+            $(`input[name=${radioArray[i]}]`).removeClass('error')
+          }
+        }
+      } 
+    })
   }
 
   validateEmail(email) {
@@ -22,4 +93,15 @@ export default class TextInput {
       return re.test(email);
     }
   }
+
+  validatePhoneNumber(phoneNumber) {
+    if (phoneNumber < 1) {
+      return true
+    } else {
+      const reg = /^(?=(?:\D*\d){10,15}\D*$)\+?[0-9]{1,3}[\s-]?(?:\(0?[0-9]{1,5}\)|[0-9]{1,5})[-\s]?[0-9][\d\s-]{5,7}\s?(?:x[\d-]{0,4})?$/;
+      return reg.test(phoneNumber)
+    }
+  }
 }
+
+
