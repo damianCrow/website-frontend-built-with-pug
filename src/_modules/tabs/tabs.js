db513e28d47a18b9e9f66bcd5a06081f10e9f392
Simon Batty <simon@psymon.co.uk>
2017-10-30 16:21:18 +0000

    Fix tab initial height.

--- src/_modules/tabs/tabs.js
+++ src/_modules/tabs/tabs.js
@@ -7,15 +7,21 @@ export default class Tabs {
     this.eachTabContent = tabsNode.getElementsByTagName('section')
     // eslint-disable-next-line prefer-destructuring
     this.tabWrapper = tabsNode.getElementsByClassName('tabs__content-wrapper')[0]
+    this.firstActiveTab = tabsNode.getElementsByClassName('tabs__content--active')[0]
 
     this.setPreferences()
   }
 
   setPreferences() {
     for (let i = 0; i < this.eachTab.length; i += 1) {
-      this.eachTabContent[i].dataset.orginalHeight = this.eachTabContent[i].offsetHeight
+      this.eachTabContent[i].setAttribute('data-orginal-height', this.eachTabContent[i].offsetHeight)
+
+      // this.eachTabContent[i].dataset.orginalHeight = this.eachTabContent[i].offsetHeight
       this.eachTab[i].addEventListener('click', e => this.toggleTab(e))
     }
+
+    this.tabWrapper.style.height = `${this.firstActiveTab.getAttribute('data-orginal-height')}px`
+    addClass(this.tabWrapper, 'tabs__content--transition')
   }
 
   toggleTab(e) {
@@ -27,8 +33,8 @@ export default class Tabs {
       removeClass(this.eachTabContent[i], 'tabs__content--active')
     }
 
-    const tabContent = this.parent.querySelector(`section[data-tab-link="${e.srcElement.dataset.tabLink}"]`)
-    this.tabWrapper.style.height = `${tabContent.dataset.orginalHeight}px`
+    const tabContent = this.parent.querySelector(`section[data-tab-link="${e.srcElement.getAttribute('data-tab-link')}"]`)
+    this.tabWrapper.style.height = `${tabContent.getAttribute('data-orginal-height')}px`
     addClass(tabContent, 'tabs__content--active')
     addClass(e.srcElement, 'tabs__tab--active')
   }
