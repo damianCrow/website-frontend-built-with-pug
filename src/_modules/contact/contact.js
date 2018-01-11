ed98ab7ea05397761db8341238ab36d487968d17
Damian Whyte <damian.w@interstateteam.com>
2018-01-11 12:10:49 +0000

    code amended to allow dynamic marker addition from json in html

--- src/_modules/contact/contact.js
+++ src/_modules/contact/contact.js
@@ -7,16 +7,20 @@ export default class Contact {
     Mapbox.accessToken = 'pk.eyJ1IjoibXJhd2Vzb21lIiwiYSI6ImNqMzJzeDN4ZjAwMHAyd250YmRyZm04MDcifQ.YcsqDP3hqulR3f523AoB1g'
     const map = new Mapbox.Map({
       container: 'map',
-      center: [mapEle.getAttribute('data-center-lat'), mapEle.getAttribute('data-center-lng')],
+      center: [mapEle.getAttribute('data-center-lng'), mapEle.getAttribute('data-center-lat')],
       zoom: 16,
       style: 'mapbox://styles/mapbox/dark-v9',
     })
 
-    const marker = document.createElement('div')
-    marker.classList.add('marker')
+    JSON.parse(mapEle.getAttribute('data-markers')).map((markerData, idx) => {
 
-    new Mapbox.Marker(marker)
-      .setLngLat([mapEle.getAttribute('data-lat'), mapEle.getAttribute('data-lng')])
+      const marker = document.createElement('div')
+      marker.setAttribute('data-content', markerData.text)
+      marker.classList.add('marker')
+
+      new Mapbox.Marker(marker)
+      .setLngLat([markerData.dataLng, markerData.dataLat])
       .addTo(map)
+    })
   }
 }
